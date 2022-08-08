7542d07df0328388025c57da4063667f92d1b568
MatheusOlca <matheus.caldas2@etec.sp.gov.br>
2022-08-08 08:55:12 -0300

    Working Fine

--- app/src/main/java/com/example/autenticador/RegisterActivity.kt
+++ app/src/main/java/com/example/autenticador/RegisterActivity.kt
@@ -1,46 +1,38 @@
 package com.example.autenticador
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     private lateinit var  auth : FirebaseAuth
     private lateinit var inputEmail : EditText
     private lateinit var inputPassword : EditText
-    private lateinit var btnLogin : Button
-    private lateinit var txtRegister : TextView
+    private lateinit var btnRegister : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
 
         auth = Firebase.auth
         inputEmail = findViewById(R.id.inputUsername)
         inputPassword = findViewById(R.id.inputPassword)
-        btnLogin = findViewById(R.id.btnLogin)
-        txtRegister = findViewById(R.id.txtRegister)
+        btnRegister = findViewById(R.id.btnRegister)
 
-        txtRegister.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
-        }
-
-        btnLogin.setOnClickListener {
-            performLogin()
+        btnRegister.setOnClickListener {
+            performRegister()
         }
 
     }
 
-    fun performLogin() {
+    fun performRegister(){
         if(inputEmail.text.isEmpty() && inputPassword.text.isEmpty()){
             Toast.makeText(this,  "Um dos campos está vazio!", Toast.LENGTH_SHORT).show()
             return
@@ -48,15 +40,14 @@ class MainActivity : AppCompatActivity() {
 
         val userEmail = inputEmail.text.toString()
         val userPassword = inputPassword.text.toString()
-
-        auth.signInWithEmailAndPassword(userEmail, userPassword)
+        auth.createUserWithEmailAndPassword(userEmail, userPassword)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    Toast.makeText(this, "Sucesso ao Logar! \uD83D\uDE00", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, PrincipalActivity::class.java)
+                    Toast.makeText(this, "Cadastrado com sucesso! \uD83D\uDE00", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                 } else {
-                    Toast.makeText(this, "Usuário e/ou senha não conferem! \uD83D\uDE1E", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Falha ao cadastrar! \uD83D\uDE1E", Toast.LENGTH_SHORT).show()
                 }
             }
     }
